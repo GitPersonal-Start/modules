bcad0a5a5d5a43a91d4f891166a27743772eb9f8
GitPersonal-Start <luisdaniel960824@gmail.com>
2024-09-08 18:54:29 -0400

    generando informacion del modulo en el manifest

--- perecedero/__manifest__.py
+++ perecedero/__manifest__.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "perecedero",
+    'name': "Gestión de Productos Perecederos",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Módulo para la gestión eficiente de productos perecederos en Cimex",
 
     'description': """
-Long description of module's purpose
+Este módulo está diseñado para ayudar a las empresas a gestionar sus productos perecederos de manera efectiva. 
+Permite el seguimiento del inventario, la gestión de fechas de caducidad, y la optimización de la rotación de productos. 
+Con herramientas para alertas de caducidad y reportes de stock, este módulo asegura que los productos frescos se manejen adecuadamente, minimizando pérdidas y mejorando la eficiencia operativa.
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Ing. Luis D. Gonzalez Sanches",
+    'website': "https://www.cimex.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Inventory',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -30,5 +32,8 @@ Long description of module's purpose
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
 
